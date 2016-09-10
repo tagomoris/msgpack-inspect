a9d69621320887f689614af49adc53d543eeab0b
TAGOMORI Satoshi <tagomoris@gmail.com>
2016-09-10 17:35:58 +0900

    add testing helper methods

--- test/helper.rb
+++ test/helper.rb
@@ -1,3 +1,15 @@
 require 'test/unit'
 require 'msgpack'
 require 'stringio'
+
+def pack(data)
+  MessagePack.pack(data)
+end
+
+def io2ins(io)
+  MessagePack::Inspect::Inspector.new(io)
+end
+
+def str2ins(str)
+  MessagePack::Inspect::Inspector.new(StringIO.new(str))
+end
